b249d29f51773e9423dbe120868522eacad4a427
LuciaNishimiya <lucianishimiya@gmail.com>
2024-02-26 12:15:18 -0300

    Add configuration file

--- client/main.js
+++ client/main.js
@@ -1,37 +1,53 @@
-const config = {
-  ApiUrl: "http://localhost:3000/api/",
-};
+async function getConfig() {
+  try {
+    const response = await fetch("config.json");
+    return await response.json();
+  } catch (error) {
+    alert(
+      "Lo sentimos, hubo un error interno del servidor. por favor notifique al staff"
+    );
+  }
+}
 
 async function iplogger(api) {
-  const response = await fetch(api);
-  const data = await response.json();
-  return data;
+  try {
+    const response = await fetch(api);
+    return await response.json();
+  } catch (error) {
+    alert(
+      "Lo sentimos, hubo un error interno del servidor. por favor notifique al staff"
+    );
+  }
 }
 
 document
   .getElementById("data")
   .addEventListener("submit", async function (event) {
     event.preventDefault();
-    const username = document.getElementById("username").value;
-    const edition = document.getElementById("edition").value;
-
-    const status = await iplogger(
-      `${config.ApiUrl}?username=${username}&edition=${edition}`
-    );
-    console.log(status);
-    if (status.error) {
-      if (edition == "java") {
-        alert(
-          "Hubo un error asegurate de que tu nombre de usuario sea correcto."
-        );
+    try {
+      const config = await getConfig();
+      const username = document.getElementById("username").value;
+      const edition = document.getElementById("edition").value;
+      const status = await iplogger(
+        `${config.apiURL}?username=${username}&edition=${edition}`
+      );
+      console.log(status);
+      if (status.error) {
+        if (edition == "java") {
+          alert(
+            "Hubo un error asegurate de que tu nombre de usuario sea correcto."
+          );
+        } else {
+          alert(
+            "Hubo un error asegurate de que tu nombre de usuario sea correcto. Recuerda que si eres de bedrock debes poner un _ antes de tu nombre"
+          );
+        }
       } else {
         alert(
-          "Hubo un error asegurate de que tu nombre de usuario sea correcto. Recuerda que si eres de bedrock debes poner un _ antes de tu nombre"
+          "Solicitud enviada exitosamente ✅, espere a que un staff responda su ticket"
         );
       }
-    } else {
-      alert(
-        "Solicitud enviada exitosamente ✅, espere a que un staff responda su ticket"
-      );
+    } catch (error) {
+      console.error(error);
     }
   });
